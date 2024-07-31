a491b986c87b33c60f0320e58eea484b7a8fa518
Sanketh abilas <sankethmarasinghaop@gmail.com>
2024-07-31 21:53:31 +0530

    finished basic ui

--- index.php
+++ index.php
@@ -7,6 +7,27 @@
 </head>
 
 <body>
+    <h1>User Registration</h1>
+
+    <p>First Name</p>
+    <input type="text" id="fname" />
+
+    <p>Last Name</p>
+    <input type="text" id="lname" />
+
+    <p>Email</p>
+    <input type="text" id="email" />
+
+    <p>Password</p>
+    <input type="text" id="pw" />
+
+    <p>City</p>
+    <input type="text" id="city" />
+
+    <br /><br />
+
+    <button onclick="signup();">Sign Up</button>
+
 
 
     <script src="script.js"></script>
